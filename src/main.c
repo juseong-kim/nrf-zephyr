df018e6927c797f78a13f2c3d5b0c3d88d81c65f
Juseong (Joe) Kim <juseong.kim@hotmail.com>
2023-04-08 13:41:21 -0400

    remove K_FIFO test

--- src/main.c
+++ src/main.c
@@ -8,18 +8,7 @@
 /* Logger */
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
-/* K_FIFO Test */
-// K_FIFO_DEFINE(fifo1);
-
-// struct data_item_t {
-// 	void *fifo_reserved;
-// 	int val;
-// };
-// struct data_item_t tx1;
-// struct data_item_t tx2;
-// struct data_item_t *rx;
-
-int vq[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+int vq[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}; // index via vq[(i1+i) % 10]
 int i1 = 0;
 
 void add_v(int q[10], int val){
@@ -46,25 +35,5 @@ void main(void)
 				}
 			}
 		}
-		
 	}
-
-	/* K_FIFO Test */
-	// for (int i=0;i<15;i++){
-	// 	LOG_DBG("i=%d", i);
-	// 	if(i==0){
-	// 		tx1.val = 1;
-	// 		k_fifo_put(&fifo1, &tx1);
-	// 	}
-	// 	else{
-	// 		tx2.val = i;
-	// 		k_fifo_put(&fifo1, &tx2);
-	// 	}
-	// 	k_msleep(100);
-
-	// 	if (i > 3) {
-	// 		rx = k_fifo_get(&fifo1, K_FOREVER);
-	// 		LOG_DBG("val=%d", rx->val);
-	// 	}
-	// }
 }
